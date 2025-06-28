ac9e1540ad121050456d0367ebb404c9d70cfded
Diamantea Mongelli <diamantea.mongelli@gmail.com>
2025-06-28 15:33:57 +0200

    Add BetController with test that check if it is instantiable

---
+++ src/main/java/com/ippobet/controller/BetController.java
@@ -0,0 +1,5 @@
+package com.ippobet.controller;
+
+public class BetController {
+    public BetController() {}
+}
---
+++ src/test/java/ippobet/controller/BetControllerTest.java
@@ -0,0 +1,16 @@
+package ippobet.controller;
+
+import com.ippobet.controller.BetController;
+import org.junit.jupiter.api.Test;
+
+public class BetControllerTest {
+    @Test
+    void testIfEventControllerIsInstantiable(){
+        var controller = new BetController();
+    }
+
+    @Test
+    void testGetAllEvents() {
+        var controller = new BetController();
+    }
+}
