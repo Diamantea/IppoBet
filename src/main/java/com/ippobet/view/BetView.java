d9f71632387365538b430f77affb1593f993425b
Diamantea Mongelli <diamantea.mongelli@gmail.com>
2025-06-28 15:52:14 +0200

    Added BetView empty class

---
+++ src/main/java/com/ippobet/view/BetView.java
@@ -0,0 +1,4 @@
+package com.ippobet.view;
+
+public class BetView {
+}
---
+++ src/test/java/ippobet/view/BetViewTest.java
@@ -0,0 +1,11 @@
+package ippobet.view;
+
+import com.ippobet.view.BetView;
+import org.junit.jupiter.api.Test;
+
+public class BetViewTest {
+    @Test
+    void testIfIsInstantiable() {
+        var view = new BetView();
+    }
+}
