c9c83d4676afbf52f9b0d27cd378450823367c30
Diamantea Mongelli <diamantea.mongelli@gmail.com>
2025-08-11 12:07:55 +0200

    Add Mongo DB in the project

--- src/test/java/com/ippobet/repository/mongo/BetMongoRepositoryTest.java
+++ src/test/java/com/ippobet/repository/mongo/BetMongoRepositoryTest.java
@@ -1,12 +1,26 @@
 package com.ippobet.repository.mongo;
 
 import com.ippobet.repository.mongo.BetMongoRepository;
+import com.mongodb.client.MongoCollection;
+import io.cucumber.java.Before;
+import org.bson.Document;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 public class BetMongoRepositoryTest
 {
+    @Mock
+    MongoCollection<Document> betCollection;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     void testIfRepositoryIsInstantiable() {
-        var repository = new BetMongoRepository();
+        var repository = new BetMongoRepository(betCollection);
     }
 }
